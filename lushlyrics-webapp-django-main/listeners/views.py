c7a8cdeaae7f0fdbf74d1335cc82b9eb01bf2008
Lawi Kiprotich <fleeklawi@gmail.com>
2023-11-16 22:44:56 +0300

    Added - Urls for the listeners app

--- lushlyrics-webapp-django-main/listeners/views.py
+++ lushlyrics-webapp-django-main/listeners/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def login_listener(request):
-    return render(request, 'listeners/login.html')
+    if request.method == 'POST':
+        username = request.POST['username'] 
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(user)
+            messages.success('Login Successful')
+            return redirect('home')
+        
+        else:
+            messages.success(request, ('Enter Valid Details'))
+            return redirect('login')
+    else:
+        return render(request, 'listeners/login.html', {})
+    
+
+def register_listener(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            messages.success("Registered Sucessful")
+            return redirect('login')
+        
+        else:
+            messages.success('Invalid form')
+    else:   
+        form = UserCreationForm()
+    
+    return render(request, 'listeners/signup.html', {'form': form})
